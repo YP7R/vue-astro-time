9595bd9628cfba2de8fe0a1cb9cd9e2ad97dd900
YP7R <perez.s.yohann@gmail.com>
2022-06-15 02:48:55 +0200

    added reactive property

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { onMounted, onBeforeUnmount, computed, reactive } from "vue";
+import { onMounted, onBeforeUnmount, computed, reactive, ref } from "vue";
 import localSideralTime from '../sideralTime'
+
 let timer = null;
-let reactiveNow = reactive({ now: Date.now() });
 let now = Date.now();
+
+let refnow = ref(Date.now())
 
 onMounted(() => {
   timer = setInterval(() => {
-    reactiveNow.now = Date.now();
+    refnow.value = Date.now();
   }, 1000);
 });
 
@@ -15,7 +17,7 @@ onBeforeUnmount(() => {
   clearInterval(timer);
 });
 
-
+// Options 
 let options = {
   weekday: "long",
   year: "numeric",
@@ -31,13 +33,23 @@ let options = {
 let optionsLocal = { ...options, timeZone: "Europe/Zurich" };
 let optionsUTC = { ...options, timeZone: "UTC" };
 
+//Computed property
 let localDate = new Date(now).toLocaleString("fr-CH", optionsLocal);
-let computedLocalDate = computed(() => {
-  new Date(reactiveNow.now).toLocaleString("fr-CH", optionsLocal);
-});
-
 let utcDate = new Date(now).toLocaleString("fr-CH", optionsUTC);
 let sideralTime = localSideralTime(6.131966, new Date(now));
+
+let computedLocalDate = computed(() => {
+  return new Date(refnow.value).toLocaleString('fr-CH', optionsLocal);
+});
+
+let computedUtcDate = computed(()=>{
+  return new Date(refnow.value).toLocaleString('fr-CH', optionsUTC);
+})
+
+let computedSiderealTime = computed(()=>{
+  return localSideralTime(6.131966, new Date(refnow.value)).toFixed(4);
+})
+
 defineProps({
   msg: {
     type: String,
@@ -47,20 +59,20 @@ defineProps({
 </script>
 
 <template>
-  <div class="w-auto h-auto">
+  <div class="w-auto h-80 my-20">
     <div class="flex flex-row p-6 bg-black text-white font-mono text-center">
       <div class="basis-1/2 w-auto">
-        <h1 class="text-2xl font-black">{{ utcDate }}</h1>
+        <h1 class="text-2xl font-black">{{ computedUtcDate }}</h1>
         <h2 class="text-base text-[#989898] font-semibold">Temps universel</h2>
       </div>
       <div class="basis-1/2 w-auto">
-        <h1 class="text-2xl font-black">{{ localDate }}</h1>
+        <h1 class="text-2xl font-black">{{ computedLocalDate }}</h1>
         <h2 class="text-base font-semibold text-[#989898]">Temps local</h2>
       </div>
     </div>
     <div class="flex flex-row p-6 bg-[#989898] text-black font-mono text-center">
       <div class="basis-1/1 w-full">
-        <h1 class="text-4xl font-black">{{ sideralTime.toFixed(4) }} [unité ?!]</h1>
+        <h1 class="text-4xl font-black">{{ computedSiderealTime }} [unité ?!]</h1>
         <h2 class="text-xl font-semibold text-white">Temps sidéral</h2>
       </div>
     </div>
