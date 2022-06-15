3cd683e73f1e8b97b6e07d56808ca5437a9a2e3c
YP7R <perez.s.yohann@gmail.com>
2022-06-15 02:09:56 +0200

    design"

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,19 @@
 <script setup>
+import { onMounted, onBeforeUnmount, computed, reactive } from "vue";
+
+let timer = null;
+let reactiveNow = reactive({ now: Date.now() });
+let now = Date.now();
+
+onMounted(() => {
+  timer = setInterval(() => {
+    reactiveNow.now = Date.now();
+  }, 1000);
+});
+
+onBeforeUnmount(() => {
+  clearInterval(timer);
+});
 
 let localSideralTime = (longitude, dateNow) => {
   let datetime = dateNow,
@@ -48,7 +63,7 @@ let sideralTimeGreewich = (julianday) => {
 
   return temp;
 };
-let now = Date.now();
+
 let options = {
   weekday: "long",
   year: "numeric",
@@ -65,23 +80,39 @@ let optionsLocal = { ...options, timeZone: "Europe/Zurich" };
 let optionsUTC = { ...options, timeZone: "UTC" };
 
 let localDate = new Date(now).toLocaleString("fr-CH", optionsLocal);
+let computedLocalDate = computed(() => {
+  new Date(reactiveNow.now).toLocaleString("fr-CH", optionsLocal);
+});
+
 let utcDate = new Date(now).toLocaleString("fr-CH", optionsUTC);
-let sideralTime = localSideralTime(6.131966, new Date(now))
+let sideralTime = localSideralTime(6.131966, new Date(now));
 defineProps({
   msg: {
     type: String,
     required: true,
   },
 });
-
-
 </script>
 
 <template>
-    <h1 class="green">UTC : {{ utcDate }}</h1>
-    <h1>Local : {{ localDate }}</h1>
-    <h1>Temps sidéral : {{ sideralTime }}</h1>
-
+  <div class="w-auto h-auto">
+    <div class="flex flex-row p-6 bg-black text-white font-mono text-center">
+      <div class="basis-1/2 w-auto">
+        <h1 class="text-2xl font-black">{{ utcDate }}</h1>
+        <h2 class="text-base text-[#989898] font-semibold">Temps universel</h2>
+      </div>
+      <div class="basis-1/2 w-auto">
+        <h1 class="text-2xl font-black">{{ localDate }}</h1>
+        <h2 class="text-base font-semibold text-[#989898]">Temps local</h2>
+      </div>
+    </div>
+    <div class="flex flex-row p-6 bg-[#989898] text-black font-mono text-center">
+      <div class="basis-1/1 w-full">
+        <h1 class="text-4xl font-black">{{ sideralTime.toFixed(4) }} [unité ?!]</h1>
+        <h2 class="text-xl font-semibold text-white">Temps sidéral</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
